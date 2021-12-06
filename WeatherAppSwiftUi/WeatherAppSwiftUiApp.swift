5695caa71dbb8b8caa897618085c2ac8cba1fe4b
Mike Andrew <mikeandrew2016@gmail.com>
2021-12-06 18:39:17 +0300

    Initial Commit

---
+++ WeatherAppSwiftUi/WeatherAppSwiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppSwiftUiApp.swift
+//  WeatherAppSwiftUi
+//
+//  Created by Meritocrat on 12/6/21.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppSwiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
